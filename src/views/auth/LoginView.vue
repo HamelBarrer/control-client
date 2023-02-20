1cedc067dbd64bb4a10ab54181e00755db0d614b
Hamel Barrero <hamel_-barrero@hotmail.com>
2023-02-20 12:28:44 -0500

    config page login

---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="max__content">
+    <div class="card">
+      <form class="form">
+        <div class="form__group">
+          <label class="form__label">Account:</label>
+          <input class="form__input" type="text" />
+        </div>
+        <div class="form__group">
+          <label class="form__label">Password:</label>
+          <input class="form__input" type="password" />
+        </div>
+        <div class="form__buttons">
+          <button class="button">Log in</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
